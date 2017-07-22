de9935cc57cde88613c7365841ed0ce7a7bcc760
Dave Crist <david_crist@bose.com>
2017-07-22 09:43:23 -0400

    fixing obvious bugs in module file JOS3.jl

--- src/JOS3.jl
+++ src/JOS3.jl
@@ -1,17 +1,8 @@
 isdefined(Base, :__precompile__) && __precompile__()
 
 module JOS3
-    export clipdb,
-           check_gradient,
-           check_hessian,
-           check_second_derivative,
-           deparse,
-           derivative,
-           differentiate,
-           hessian,
-           jacobian,
-           second_derivative
+    export clipdb, fold, mps
     include("./filters/clipdb.jl")
     include("./filters/fold.jl")
-    include("./filters/mps.jl")    
+    include("./filters/mps.jl")
 end
