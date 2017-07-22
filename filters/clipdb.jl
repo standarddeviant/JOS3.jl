ccf9b58a75732540e8ea2b4089407436a8a8ef85
Dave Crist <david_crist@bose.com>
2017-07-22 04:41:44 -0400

    untested attempt of translating clipdb

---
+++ filters/clipdb.jl
@@ -0,0 +1,18 @@
+function clipdb(s,cutoff)
+  # [clipped] = clipdb(s,cutoff)
+  # Clip magnitude of s at its maximum + cutoff in dB.
+  # Example: clip(s,-100) makes sure the minimum magnitude
+  # of s is not more than 100dB below its maximum magnitude.
+  # If s is zero, nothing is done.
+
+  clipped = s;
+  as = abs(s);
+  mas = maximum(as);
+  if mas==0; return; end
+  if cutoff >= 0; return; end
+
+  thresh = mas*10^(cutoff/20); # db to linear
+  clipped = s;
+  clipped[ as .< thresh ] = thresh;
+  return clipped;
+end
