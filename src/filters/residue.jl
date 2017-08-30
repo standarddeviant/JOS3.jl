3f055f731616ddabd8f17265737b824c6d8b5669
Dave Crist <david_crist@bose.com>
2017-08-30 01:13:49 -0400

    It looks like repeated roots are handled for obtaining abc_polys

--- src/filters/residue.jl
+++ src/filters/residue.jl
@@ -19,10 +19,29 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 =#
 
+function uniquetol_map(itr,tol=1e-6)
+    # utVals = Array{Number}(0)
+    ut_vals_map = Dict()
+    ut_idxs_map = Dict()
+    if length(itr) == 0
+        return utMap
+    end
+
+    _itr = copy(itr)
+    while(length(_itr) > 0)
+        idxs = find(abs.(itr - _itr[1]) .< tol)
+        utval = mean(itr[idxs])
+        ut_vals_map[ utval ] = itr[idxs]
+        ut_idxs_map[ utval ] =     idxs
+        deleteat!(_itr,find(abs.(_itr - _itr[1]) .< tol))
+    end
+    
+    return ut_vals_map, ut_idxs_map
+end
+
 # residue.jl
 using Polynomials
-using StatsBase
-function residue(pn::Poly, pd::Poly)
+function residue(pn::Poly, pd::Poly, root_tol=1e-6)
     # calculate degree and roots of denominator
     degn = degree(pn)
     degd = degree(pd)
@@ -35,33 +54,42 @@ function residue(pn::Poly, pd::Poly)
         pq=Poly(0); pr=copy(pn)
     end
 
-    # get roots, and counts of repeated roots as keys and values of a
-    # map or dictionary object
+    # get roots, and find roots that are numerically close
+    # rootsd = roots of denominator polynomial
     rootsd = sort(roots(pd))
-    # rootsd_map = countmap(rootsd_all)
+    # rd_ut_idxs = groups of idxs in to rootsd, 
+    #     that represent numerically close roots
+    rd_ut_vals, rd_ut_idxs = uniquetol_map(rootsd,1e-6)
 
-    # Loop over denominator roots
-    # rD = 'current' root of denominator we're dealing with
-    # rL = root for looping over other roots
+    # Loop over denominator roots, in groups, determined by uniquetol_map
+    # uniquetol_map is used to find roots that are numerically close but uneqal
     # abc_polys are the polynomials multiplying A, B, C, etc.
     # in the form
     # (Numerator-Poly) = A*abc_poly[1] + B*abc_poly[2] + C*abc_poly[3] + ...
-    abc_polys = [] #[Poly(0) for idx = 1:length(rootsd)]
-    for rD = unique(rootsd)
+    abc_polys = []
+
+    for rdidxs = values(rd_ut_idxs)
+        # @show rdidxs
         # Account for all other-valued roots
-        abc_poly = prod( [Poly([-rL,1]) for rL in rootsd if rL!=rD] )            
+        other_roots = rootsd[setdiff( 1:length(rootsd), rdidxs)]
+        abc_poly = prod( [Poly([-rL,1]) for rL in other_roots] )
+        # print("rD=$(rootsd[rdidxs[1]]) , (normal): $abc_poly\n")
         push!(abc_polys, abc_poly)
 
-        # Account for repeated roots equal to rD
-        rD_reps = sum( rootsd .== rD)
-        # the denominator has rD_reps occurrences of this root
-        for rep = 2:rD_reps # start at 2 to ignore non-repeated roots
+        # Account for repeated roots numerically close to to rD
+        for repidx = 2:length(rdidxs) # start at 2 to ignore non-repeated roots
+            rD = rootsd[rdidxs[repidx]]
+            # print("rD=$rD , (BEFORE) rep=$repidx: $abc_poly\n")            
             abc_poly *= Poly([-rD,1])
+            # print("rD=$rD , rep=$repidx: $abc_poly\n")
             push!(abc_polys, abc_poly)
         end
     end
 
     # This *should* set up a nice set of linear equations for 
     # coefficients of NumCoef * x^pow = (A+B+C+...) * x^pow
+    return abc_polys, rootsd
 
 end # residue function
+
+
